1f7c41c60f2896713dfc5d3a6ee8f243815f5743
Antonio Cuenco <acuenco@atlascarbon.ag>
2024-12-15 00:03:21 +1000

    Initial Commit

---
+++ statebucket/variables.tf
@@ -0,0 +1,14 @@
+###############################################################################
+# Environment
+###############################################################################
+variable "region" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
